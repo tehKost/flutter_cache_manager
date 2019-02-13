ed8003a3d27a5b0587a1f1993fa6f5d9050c6d99
Kost Kunica <xxxkostxxx@gmail.com>
2019-02-14 01:04:34 +0300

    Update cache_store.dart

--- lib/src/cache_store.dart
+++ lib/src/cache_store.dart
@@ -14,10 +14,10 @@ import 'package:synchronized/synchronized.dart';
 class CacheStore {
   Map<String, Future<CacheObject>> _memCache = new Map();
 
-  int _nrOfDbConnections = 0;
   Future<String> filePath;
   Future<CacheObjectProvider> _cacheObjectProvider;
   String storeKey;
+  final _lock = Lock();
 
   int _capacity;
   Duration _maxAge;
@@ -77,59 +77,40 @@ class CacheStore {
   }
 
   Future<CacheObject> _getCacheDataFromDatabase(String url) async {
-    var provider = await _openDatabaseConnection();
+    var provider = await _getProvider();
     var data = await provider.get(url);
     if (await _fileExists(data)) {
       _updateCacheDataInDatabase(data);
     }
-    _closeDatabaseConnection();
     return data;
   }
 
   Future<dynamic> _updateCacheDataInDatabase(CacheObject cacheObject) async {
-    var provider = await _openDatabaseConnection();
+    var provider = await _getProvider();
     var data = await provider.updateOrInsert(cacheObject);
-    _closeDatabaseConnection();
     return data;
   }
 
-  var databaseConnectionLock = Lock();
-  Future<CacheObjectProvider> _openDatabaseConnection() async {
+  Future<CacheObjectProvider> _getProvider() async {
     var provider = await _cacheObjectProvider;
-    if (_nrOfDbConnections == 0) {
-      await databaseConnectionLock.synchronized(() async {
-        if (_nrOfDbConnections == 0) {
+    if (provider.db == null) {
+      await _lock.synchronized(() async {
+        if (provider.db == null) {
           await provider.open();
+          _clean();
         }
-        _nrOfDbConnections++;
       });
-    } else {
-      _nrOfDbConnections++;
     }
     return provider;
   }
 
-  _closeDatabaseConnection() async {
-    if (_nrOfDbConnections == 1) {
-      await databaseConnectionLock.synchronized(() {
-        _nrOfDbConnections--;
-        if (_nrOfDbConnections == 0) {
-          _cleanAndClose();
-        }
-      });
-    } else {
-      _nrOfDbConnections--;
-    }
-  }
-
-  _cleanAndClose() async {
-    _nrOfDbConnections++;
+  _clean() async {
     var provider = await _cacheObjectProvider;
-    var overCapacity = await provider.getObjectsOverCapacity(_capacity);
+    var overCapactity = await provider.getObjectsOverCapacity(_capacity);
     var oldObjects = await provider.getOldObjects(_maxAge);
 
     var toRemove = List<int>();
-    overCapacity.forEach((cacheObject) async {
+    overCapactity.forEach((cacheObject) async {
       _removeCachedFile(cacheObject, toRemove);
     });
     oldObjects.forEach((cacheObject) async {
@@ -137,31 +118,24 @@ class CacheStore {
     });
 
     await provider.deleteAll(toRemove);
-    await databaseConnectionLock.synchronized(() async {
-      _nrOfDbConnections--;
-      if (_nrOfDbConnections == 0) {
-        await provider.close();
-      }
-    });
   }
 
   removeCachedFile(CacheObject cacheObject) async {
-    var provider = await _openDatabaseConnection();
+    var provider = await _getProvider();
     var toRemove = List<int>();
     _removeCachedFile(cacheObject, toRemove);
     await provider.deleteAll(toRemove);
-    _closeDatabaseConnection();
   }
 
   _removeCachedFile(CacheObject cacheObject, List<int> toRemove) async {
     if (!toRemove.contains(cacheObject.id)) {
-      toRemove.add(cacheObject.id);
-      if (_memCache.containsKey(cacheObject.url))
-        _memCache.remove(cacheObject.url);
-    }
-    var file = new File(p.join(await filePath, cacheObject.relativePath));
-    if (await file.exists()) {
-      file.delete();
+      var file = new File(p.join(await filePath, cacheObject.relativePath));
+      if (await file.exists()) {
+        toRemove.add(cacheObject.id);
+        file.delete();
+        if (_memCache.containsKey(cacheObject.url))
+          _memCache.remove(cacheObject.url);
+      }
     }
   }
 }
